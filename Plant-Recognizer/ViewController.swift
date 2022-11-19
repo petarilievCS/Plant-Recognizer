976c1bdfbd9152bbd5c157d056e0730d1a42e4d7
Petar Iliev <petariliev2002@gmail.com>
2022-11-19 11:44:20 -0600

    Implement classification from CoreML model

--- Plant-Recognizer/ViewController.swift
+++ Plant-Recognizer/ViewController.swift
@@ -6,20 +6,22 @@
 //
 
 import UIKit
+import CoreML
+import Vision
 
 class ViewController: UIViewController, UIImagePickerControllerDelegate, UINavigationControllerDelegate {
     
     @IBOutlet weak var imageView: UIImageView!
     
     var camera : UIImagePickerController?
-
+    
     override func viewDidLoad() {
         super.viewDidLoad()
         camera = UIImagePickerController()
         camera?.delegate = self
         camera?.sourceType = .camera
     }
-
+    
     @IBAction func cameraPressed(_ sender: UIBarButtonItem) {
         guard let safeCamera = camera else {
             fatalError("Camera picker not initialized")
@@ -27,10 +29,46 @@ class ViewController: UIViewController, UIImagePickerControllerDelegate, UINavig
         present(safeCamera, animated: true)
     }
     
+    // image taken
     func imagePickerController(_ picker: UIImagePickerController, didFinishPickingMediaWithInfo info: [UIImagePickerController.InfoKey : Any]) {
-        let image = info[UIImagePickerController.InfoKey.originalImage]
-        imageView.image = image as? UIImage
+        if let image = info[UIImagePickerController.InfoKey.originalImage] as? UIImage {
+            imageView.image = image
+            guard let coreImage = CIImage(image: image) else {
+                fatalError("Failed to convert image to core image")
+            }
+            detect(coreImage)
+        }
+        
         camera?.dismiss(animated: true)
+    }
+    
+    // classify image
+    func detect(_ image: CIImage) {
+        
+        // create model
+        guard let model = try? VNCoreMLModel(for: FlowerClassifier().model) else {
+            fatalError("Couldn't initialize CoreML model")
+        }
+        
+        // create request
+        let request = VNCoreMLRequest(model: model) { request, error in
+            guard let results = request.results as? [VNClassificationObservation] else {
+                fatalError("Error while accessing results")
+            }
+            if let firstResult = results.first {
+                DispatchQueue.main.async {
+                    self.navigationItem.title = firstResult.identifier
+                }
+            }
+        }
+        
+        // perform request, update UI
+        let handler = VNImageRequestHandler(ciImage: image)
+        do {
+            try handler.perform([request])
+        } catch {
+            print("Error while performing request")
+        }
     }
     
 }
