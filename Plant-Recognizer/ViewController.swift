1e583798247542d7b80be319ddea343f5e56731a
Petar Iliev <petariliev2002@gmail.com>
2022-11-19 11:31:28 -0600

    Implement camera functionality

--- Plant-Recognizer/ViewController.swift
+++ Plant-Recognizer/ViewController.swift
@@ -7,13 +7,31 @@
 
 import UIKit
 
-class ViewController: UIViewController {
+class ViewController: UIViewController, UIImagePickerControllerDelegate, UINavigationControllerDelegate {
+    
+    @IBOutlet weak var imageView: UIImageView!
+    
+    var camera : UIImagePickerController?
 
     override func viewDidLoad() {
         super.viewDidLoad()
-        // Do any additional setup after loading the view.
+        camera = UIImagePickerController()
+        camera?.delegate = self
+        camera?.sourceType = .camera
     }
 
-
+    @IBAction func cameraPressed(_ sender: UIBarButtonItem) {
+        guard let safeCamera = camera else {
+            fatalError("Camera picker not initialized")
+        }
+        present(safeCamera, animated: true)
+    }
+    
+    func imagePickerController(_ picker: UIImagePickerController, didFinishPickingMediaWithInfo info: [UIImagePickerController.InfoKey : Any]) {
+        let image = info[UIImagePickerController.InfoKey.originalImage]
+        imageView.image = image as? UIImage
+        camera?.dismiss(animated: true)
+    }
+    
 }
 
